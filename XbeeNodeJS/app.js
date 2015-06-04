d9d117c46eca9ad7bf1b4b066be3ec00574ff81b
tjm5114 <tjm5114@gmail.com>
2015-06-04 16:33:22 -0400

    Line by Line Read Trial

--- XbeeNodeJS/app.js
+++ XbeeNodeJS/app.js
@@ -2,6 +2,7 @@ var util = require('util');
 var SerialPort = require('serialport').SerialPort;
 var xbee_api = require('xbee-api');
 var fs = require('fs');
+var Liner = require('liner');
 var C = xbee_api.constants;
 
 var xbeeAPI = new xbee_api.XBeeAPI({
@@ -13,25 +14,36 @@ var serialport = new SerialPort("/dev/ttyAMA0", {
     parser: xbeeAPI.rawParser()
 });
 
-serialport.on("open", function () {
-    fs.readFile('test.txt', function (err,data) {
-	var frame_obj = {
-        	type: 0x10, 
-        	id: 0x01, 
+serialport.on("open", function () { 
+	// var liner = new Liner('test.txt');
+	// liner.on('readable', function () {
+		// while (true) {
+		// var line = liner.read();
+		// if (line === null) break;
+		//Do something with line.
+		// console.log(line);
+		// }
+	// });
+	
+	// liner.on('error', function (err) {
+		// console.error(err);
+	// });
+
+	// liner.on('end', function () {
+		// process.exit(0);
+	// });
+	var data ="";
+		var frame_obj = {
+			type: 0x10, 
+    		id: 0x01, 
        		//destination64: "0013A200407A25B5",
         	broadcastRadius: 0x00,
         	options: 0x00, 
         	data: data
-        };
-
-	serialport.write(xbeeAPI.buildFrame(frame_obj));
+  		};
+		serialport.write(xbeeAPI.buildFrame(frame_obj));
         console.log('Sent to serial port the following data: ' + data);
-     });	
-	
-    
-    
-});
-
+});		
 serialport.on('data', function (data) {
     console.log('data received: ' + data);
 });
