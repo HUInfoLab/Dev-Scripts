147402df8c5882b1e0f2c204c27559eaca59d207
david-nesbeth <david.nesbeth@gmail.com>
2015-06-04 19:45:48 -0400

    Database Update
    Now read from file line by line, parses each line, stores new variables
    into database, and reads each parsed variable from database in sets

--- XbeeNodeJS/app.js
+++ XbeeNodeJS/app.js
@@ -3,42 +3,7 @@ var SerialPort = require('serialport').SerialPort;
 var xbee_api = require('xbee-api');
 var fs = require('fs');
 var C = xbee_api.constants;
-
-
-// config files
-var mongoose = require('mongoose');
-var database = require('./config/database');
-mongoose.connect(database.url); // connect to our database
-
-
-var data = "SunlightTest"; //test value for SensorVal
-var id = "TestID"; //test value for SensorID
-//fs.readFile('./data/test.txt', "utf-8", function (err, data) {
-    //setup database variables
-    var sunlightSchema = mongoose.Schema({
-        SensorID: String,
-        SensorVal: String
-    })
-    var myvalue = mongoose.model('myvalue', sunlightSchema)
-  
-    //Write to DB
-/*    var dataWrite = new myvalue({ SensorID: id, SensorVal: data })
-    dataWrite.save(function (err, dataWrite) {
-        if (err) return console.error(err);
-    });
- */ 
-    //Read from DB
-    myvalue.find(function (err, myvalue) {
-      if (err) return console.error(err);
-      console.log(myvalue[0].SensorID);
-    })
-    
-
-    
-    
-
-//});
-
+var liner = require('liner');
 
 
 /*
@@ -49,31 +14,74 @@ var xbeeAPI = new xbee_api.XBeeAPI({
 var serialport = new SerialPort("/dev/ttyAMA0", {
     baudrate: 9600,
     parser: xbeeAPI.rawParser()
+}); 
+*/
+
+
+// config files
+var mongoose = require('mongoose');
+var database = require('./config/database');
+mongoose.connect(database.url); // connect to our database
+
+
+//setup database variables
+var sunlightSchema = mongoose.Schema({
+    SensorID: String,
+    SensorVal: String
+})
+var myvalue = mongoose.model('myvalue', sunlightSchema) 
+
+
+//read file line by line
+var liner = new liner('./data/test.txt');
+liner.on('readable', function () {
+    while (true) {
+        var fileContents = liner.read();
+		if (fileContents === null) break;
+        
+        //parse contents
+        var dataArray = fileContents.split(" ");
+        var id = dataArray[0];
+        var data = dataArray[1];
+
+        //Write to DB
+        var dataWrite = new myvalue({ SensorID: id, SensorVal: data })
+        dataWrite.save(function (err, dataWrite) {
+            if (err) return console.error(err);
+        });
+    }
+});
+	
+
+liner.on('error', function (err) {
+    console.error(err);
 });
 
+//prevents from writing to database for some reason
+//liner.on('end', function () {
+//    process.exit(0);
+//});
 
+
+//Read from DB
+var x = 0;
+myvalue.find(function (err, myvalue) {
+    if (err) return console.error(err);
+    while (x<5) {   //need to improve iteration condition
+        console.log(myvalue[x].SensorID);
+        console.log(myvalue[x].SensorVal);
+        x++
+    };
+}) 
+
+
+
+
+/*
 serialport.on("open", function () {
     
-    	// var liner = new Liner('test.txt');
-	// liner.on('readable', function () {
-		// while (true) {
-		// var line = liner.read();
-		// if (line === null) break;
-		//Do something with line.
-		// console.log(line);
-		// }
-	// });
-	
-	// liner.on('error', function (err) {
-		// console.error(err);
-	// });
-
-	// liner.on('end', function () {
-		// process.exit(0);
-	// });
-    
-    ////////////fs.readFile('./data/test.txt', function (err,data) {
-	var frame_obj = {
+    fs.readFile('./data/test.txt', function (err,data) {
+	   var frame_obj = {
         	type: 0x10, 
         	id: 0x01, 
        		//destination64: "0013A200407A25B5",
@@ -82,36 +90,17 @@ serialport.on("open", function () {
         	data: data
         };
 
-	serialport.write(xbeeAPI.buildFrame(frame_obj));
+	   serialport.write(xbeeAPI.buildFrame(frame_obj));
         console.log('Sent to serial port the following data: ' + data);
-     });	
-	
+     });		
     
     //Write to DB
-    var sunlightSchema = mongoose.Schema({
-        xxx: String
-    })
-    var myvalue = mongoose.model('myvalue', sunlightSchema)
-    var dataWrite = new myvalue({ xxx: data })
-    dataWrite.save(function (err, dataWrite) {
-        if (err) return console.error(err);
-    });
-    
-    
-    
     //Read from DB and display to file
-    
-    myvalue.find(function (err, myvalue) {
-        if (err) return console.error(err);
-        console.log(myvalue)
-        console.log(data)
-    })
-    
-    
+      
 });
+*/
 
-
-
+/*
 serialport.on('data', function (data) {
     console.log('data received: ' + data);
 });
